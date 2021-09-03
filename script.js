a26f0e3837d84ab1ef67fa1b2e2fb42ecfe8b092
Daniel Sabagh <70744389+LeinadoDaVinci@users.noreply.github.com>
2021-09-03 14:09:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,17 +14,14 @@ function buttonSearch()
 
         .then((resp) => resp.json())
         .then(function (data) {
-            document.getElementById("autocomplete").innerHTML = data.StopLocation[0].name; //Reveals Station Name
+            document.getElementById("autocomplete").innerHTML = data.StopLocation[0].name; //Reveals Station Name for finality
             let hpid = data.StopLocation[0].id; //Takes Station ID
             Departures(hpid, distance); //Calls on Station's Departure Board and pushes forth Distance
-            setInterval( function() {Departures(hpid);}, 60000); //Interval Timer for Departure Board
-        })
-        .catch(function (error) {
-            console.log(error);
+            setInterval( function() {Departures(hpid, distance);}, 60000); //Interval Timer for Departure Board
         })
 }
 
-function stationName()//Reveals Selected Station Name on change
+function stationName()//Reveals Selected Station Name on change ('Autocomplete')
 {
     var destination = document.getElementById("hallplatsID").value; //Repeated in order to continue fetch
     fetch(`https://cors-anywhere.herokuapp.com/https://api.resrobot.se/v2/location.name?key=8e479caa-3d38-43f3-92e4-a3a31940e466&input=${destination}&format=json&maxNo=7`)
@@ -33,50 +30,50 @@ function stationName()//Reveals Selected Station Name on change
         .then(function (data) {
             document.getElementById("autocomplete").innerHTML = data.StopLocation[0].name; //Sets Name
         })
-        .catch(function (error) {
-            console.log(error);
-        })
 }
 
 //Liljeholmen ID = 740004046 
 //https://api.resrobot.se/v2/location.name?key=8e479caa-3d38-43f3-92e4-a3a31940e466&input=liljeholmen&format=json&maxNo=7
 function Departures(hpid, distance) // Fetches data from the station's departureboard
 { 
-    fetch(`https://cors-anywhere.herokuapp.com/https://api.resrobot.se/v2/departureBoard?key=b6df87b8-91e5-43c3-8fb5-971c5e17bf69&id=${hpid}&maxJourneys=1&format=json`)
+    var cellList = document.getElementById("table-gen"); //Unordered List
+    cellList.innerHTML = ""; //Clean upon Input
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.resrobot.se/v2/departureBoard?key=b6df87b8-91e5-43c3-8fb5-971c5e17bf69&id=${hpid}&maxJourneys=10&format=json`)
     .then(resp => resp.json())
     .then(data => 
     {
         data.Departure.forEach(result => 
         {
-            //Minutes contains the calculated amount of minutes before Departure.
-            var minutes = Math.floor(Math.abs(new Date(Date.parse(`${result.Stops.date}T${result.Stops.time}`)) - new Date())/1000/60);
-            var depdis = departedDistance(minutes, distance); // Compares distance in min to departure
-            var cellList = document.getElementById("table-gen"); //Unordered List
-            tablegen(result, minutes, cellList, depdis); //Should Generate Departure Board
+            // In order, Departure Time Calculation, then calling Table-Gen 
+            var today = new Date();
+            var dep = new Date(Date.parse(`${result.date}T${result.time}`))
+            var diffMs = (dep - today) //Checks the difference in ms
+            var minutes = Math.floor(diffMs / 60000); //Minutes contains the calculated amount of minutes before Departure.
+            var depdis = departedDistance(minutes, distance); // Checks if user can reach the station or not
+            tablegen(result, minutes, cellList, depdis); //Departures List Generator
         }
         )
     })
 }
 function departedDistance(minutes, distance) //Compares distance in min to departure
 {
-    if((Math.floor(minutes - distance)) < 0)
+    if(Math.floor(minutes - distance) < 0)
     {
-        return `Du hinner ej!`
+        return `Nope!`
     }
     else
     {
-        return `Om ${Math.floor(minutes - distance)} min!` //Currently can produce NaN
+        return `Börja dra om ${Math.floor(minutes - distance)} min!`
     }
 }
 
 function tablegen(result, minutes, cellList, depdis){
-        // cellList.innerHTML = ""; //Clean upon Input
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= 1; i++) {
         var cell = document.createElement("li"); // ListItems
 
 
-        cell.innerHTML = `Linje: ${result.Stops.transportNumber} / Åker Till: ${result.Stops.direction} / Avgång: ${minutes} min <br>
-        När du hinner dit: ${depdis}`;
+        cell.innerHTML = `Linje: ${result.transportNumber} / Åker Till: ${result.direction} / Avgång: ${minutes} min <br>
+        Kan du hinna dit?: ${depdis}`;
         cell.style.color = "gold";
         cell.style.backgroundColor = "black"
         cell.style.padding = "5px"
